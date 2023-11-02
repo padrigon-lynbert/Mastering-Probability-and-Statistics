ea8901463abe1da6393f1202d25842dd3f36cd4f
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-11-02 12:47:23 +0800

    practice

--- practice.test.py
+++ practice.test.py
@@ -3,6 +3,21 @@ from disable_warning import disable_warnings as dw
 
 A = set(numpy.arange(1,10,2)) 
 B = set(numpy.arange(0,10,2))
+C = set(numpy.arange(0,10,3))
+print(A, B, C)
+
+print("\nUnions")
+print(A.union(B))
+print(A.union(C))
+
+print("\nIntersections")
+print(f"{A.intersection(C)} {B.intersection(C)}")
+
+print("\nDifference")
+print(f"{A.difference(C)}")
+
+print("\nTest.")
+print(f"A:{A}, C: {C}")
+print(f"\nUnion: {A.union(C)}, Intersection: {A.intersection(C)}, Difference: {A.difference(C)}")
 
 dw()
-print(A, B)

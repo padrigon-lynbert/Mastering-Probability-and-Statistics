8a5e093a1ca579a1e3963d3a72e54bdb9b942537
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-12-25 17:25:28 +0800

    added a submodule(unfinished)

--- 03 Linear Regression/linear_regression.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import linear_model
-
-df = pd.read_csv("DataCSV\\LinearRegression\\home_prices.csv")
-print(df)
-
-

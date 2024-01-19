54cb00fdef1fc3beb19dc5c391fa31b5d89c85d3
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-19 10:19:37 +0800

    imported pandas repo as submodule

--- 01 Pandas/11  Reshape dataframe using melt/main11.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('11  Reshape dataframe using melt/weather.csv')
-df = pd.melt(df, id_vars=['day'], var_name="city", value_name='temperature')
-df
-df[df['city']=='chicago']
